f1e476561d68c69d1ff17429e011dfab732e4ca9
Arshia Ghafghazian <arshia.balbaldorre@gmail.com>
2024-03-05 13:06:09 +0330

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HomePage from "./pages/HomePage"
+
+const App = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+
+
+const HomePage = () => {
+    return (
+        <h1>Crypto App</h1>
+    )
+}
+
+export default HomePage
