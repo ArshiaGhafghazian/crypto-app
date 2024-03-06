a3b2bc5c9ff357212c7c6444dceb5dd202531b42
Arshia Ghafghazian <arshia.balbaldorre@gmail.com>
2024-03-06 13:08:18 +0330

    Add Table Component

---
+++ src/components/Table.tsx
@@ -0,0 +1,58 @@
+import { CurrencyType } from "../pages/HomePage"
+import CoinsData from "../types/CoinsData.type"
+import styles from "./Table.module.css"
+
+type TableProps = {
+    coins: CoinsData[]
+    currency: CurrencyType
+    isLoading: boolean
+
+}
+
+
+const Table = ({ coins, currency, isLoading }: TableProps) => {
+
+
+
+
+
+    return (
+        <div className={styles.container}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Coin</th>
+                        <th>Name</th>
+                        <th>Price <span>({currency})</span></th>
+                        <th>24H</th>
+                        <th>Total Volume</th>
+                    </tr>
+                </thead>
+                {isLoading ? "Loading..." :
+                    (<tbody>
+                        {coins.map(coin => (
+                            <tr key={coin.id}>
+                                <td>
+                                    <div className={styles.symbolContainer}>
+                                        <img src={coin.image} alt={coin.name} width={20} />
+                                        <p>{coin.symbol.toUpperCase()}</p>
+                                    </div>
+                                </td>
+                                <td>{coin.name}</td>
+                                <td>$ {coin.current_price.toLocaleString()}</td>
+                                <td >
+                                    <span className={`${styles.priceChange} ${coin.price_change_percentage_24h > 0 ? styles.increase : styles.decrease}`}>% {coin.price_change_percentage_24h.toFixed(2)}</span>
+                                </td>
+                                <td>$ {coin.total_volume.toLocaleString()}</td>
+                            </tr>
+                        ))}
+                    </tbody>)}
+
+
+            </table>
+        </div>
+    )
+}
+
+
+export default Table
