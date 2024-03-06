8c854699f559963bfaf5e15d6639d6f29559e2ff
Arshia Ghafghazian <arshia.balbaldorre@gmail.com>
2024-03-06 13:08:31 +0330

    HomePage created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,25 @@
 
+import { useState } from "react";
+import { useFetchData } from "../hooks/useFetchData"
+import Table from "../components/Table";
+
+export type CurrencyType = "usd" | "eur"
+
 
 const HomePage = () => {
+    const [pageNumber, setPageNumber] = useState<number>(1)
+    const [currency, setCurrency] = useState<CurrencyType>("usd")
+
+    const { data, isLoading } = useFetchData(pageNumber, currency)
+
+
+
+
     return (
-        <h1>Crypto App</h1>
+        <>
+            <h1>Crypto App</h1>
+            <Table coins={data} currency={currency} isLoading={isLoading} />
+        </>
     )
 }
 
